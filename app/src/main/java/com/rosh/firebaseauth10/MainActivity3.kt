6b4330590741fd5b57db6b58722187cc6dfd7a5b
user <rahmatullayev845@gmail.com>
2023-03-24 17:57:36 +0500

    auth_demo

---
+++ app/src/main/java/com/rosh/firebaseauth10/MainActivity3.kt
@@ -0,0 +1,11 @@
+package com.rosh.firebaseauth10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
